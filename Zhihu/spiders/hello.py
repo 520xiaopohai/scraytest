df6673cb33fc7890284410487d57ff39a4981393
hello <hello@hellodeiMac.local>
2017-11-06 17:14:44 +0800

    2

--- Zhihu/spiders/hello.py
+++ Zhihu/spiders/hello.py
@@ -11,40 +11,42 @@ class HelloSpider(scrapy.Spider):
 
 
         """
-        获取文章列表的URL 并交给解析函数进行具体字段的解析
-        获取下一页的URL 交给scrapy下载
+        获取文章列表的URL 并交给scrapy下载后给解析函数进行具体字段的解析
+        获取下一页的URL 交给scrapy下载 下载完成后交给parse
 
-        :param response:
-        :return:
         """
+        post_urls = response.css(".floated-thumb .posted-thumb a::attr(href)")
+        for post_url in post_urls:
+            print(post_url)
 
-        re_selector = response.xpath('//*[@id="post-112812"]/div[1]/h1/text()')
-        re2_selector = response.xpath('/html/body/div[1]/div[3]/div[1]/div[1]/h1/text()')
-        re3_selector = response.xpath('//div[@class="entry-header"]/h1/text()')
-        title = re_selector.extract()[0]
-        create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace("·","").strip()
-        post_up = response.xpath('//span[contains(@class,"vote-post-up")]/h10/text()').extract()[0]
-
-        # fav_nums = response.xpath('//span[contains(@class,"bookmark-btn")]/text()')
-        # match_re = re.match(r".*(\d+).*",fav_nums)
-        # if match_re:
-        #     fav_nums = match_re.group(1)
-        # else:
-        #     fav_nums = 0
         #
-        # comment_nums = response.xpath('//a[@href="#article-comment"]/span/text()')
-        # match2_re = re.match(r".*(\d+).*", fav_nums)
-        # if match2_re:
-        #     comment_nums = match2_re.group(1)
-        # else:
-        #     comment_nums = 0
-
-        content = response.xpath('//div[@class = "entry"]').extract()[0]
-        tag_list = create_date = response.xpath('//a[@href="article-comment"]/span').extract()
-
-        #通过css选择器提取字段
-        # title=  response.css(".entry-header h1::text").extract()[0]
-        #create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
-        # vote-post-up = response.css(".vote-post-up h10").extract()[0]
+        # re_selector = response.xpath('//*[@id="post-112812"]/div[1]/h1/text()')
+        # re2_selector = response.xpath('/html/body/div[1]/div[3]/div[1]/div[1]/h1/text()')
+        # re3_selector = response.xpath('//div[@class="entry-header"]/h1/text()')
+        # title = re_selector.extract()[0]
+        # create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace("·","").strip()
+        # post_up = response.xpath('//span[contains(@class,"vote-post-up")]/h10/text()').extract()[0]
+        #
+        # # fav_nums = response.xpath('//span[contains(@class,"bookmark-btn")]/text()')
+        # # match_re = re.match(r".*(\d+).*",fav_nums)
+        # # if match_re:
+        # #     fav_nums = match_re.group(1)
+        # # else:
+        # #     fav_nums = 0
+        # #
+        # # comment_nums = response.xpath('//a[@href="#article-comment"]/span/text()')
+        # # match2_re = re.match(r".*(\d+).*", fav_nums)
+        # # if match2_re:
+        # #     comment_nums = match2_re.group(1)
+        # # else:
+        # #     comment_nums = 0
+        #
+        # content = response.xpath('//div[@class = "entry"]').extract()[0]
+        # tag_list = create_date = response.xpath('//a[@href="article-comment"]/span').extract()
+        #
+        # #通过css选择器提取字段
+        # # title=  response.css(".entry-header h1::text").extract()[0]
+        # #create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        # # vote-post-up = response.css(".vote-post-up h10").extract()[0]
 
         pass
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from scrapy.cmdline import execute
 import sys
 import os
+"""
+https://nkcoder.github.io/2015/11/17/Scrapy-crawl-intro-install-and-config/
+"""
 print(os.path.dirname(os.path.abspath(__file__)))
 execute(["scrapy","crawl","hello"])
